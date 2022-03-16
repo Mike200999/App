fe02400b48a54080c6636b19e189d8f491d9d6aa
Michele <Michele@gmail.com>
2022-03-16 10:58:44 +0100

    vediamo

---
+++ PrimaApp/src/main/java/it/primaApp/entity/Entity22.java
@@ -0,0 +1,14 @@
+package it.primaApp.entity;
+
+public class Entity22 {
+	
+	private Integer variabile;
+
+	public Integer getVariabile() {
+		return variabile;
+	}
+
+	public void setVariabile(Integer variabile) {
+		this.variabile = variabile;
+	}
+}
